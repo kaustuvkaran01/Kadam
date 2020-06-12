c63ab61c984ce3f27457a05a84713b296d5a3f98
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2020-06-12 23:40:48 +0530

    Navbar Init

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import { AiOutlineHome } from 'react-icons/ai';
 
 
 function Navbar() {
   return (
     <NavbarContainer>
-        <p>
-            This is the Navbar component
-        </p>
+        <a href style={{paddingLeft:"2.5rem"}}>Home</a>
+        <a href>Something</a>
+        <a href>Homeset</a>
+        <a href style={{paddingRight:"2.5rem"}}>Somethingset</a>
     </NavbarContainer>
   );
 }
 export default Navbar;
 
 const NavbarContainer = styled.div`
-    display: flex;
-    background: #32302F;
-    color: #ECC731;
-    height: 10vh;
-    width: 100%;
+  display: flex;
+  flex-direction: row;
+  background: grey;
+  color: #ecc731;
+  height: 10vh;
+  width: 100%;
+    margin: 0 auto;
+    justify-content: space-between;
 `;
